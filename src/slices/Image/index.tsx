7fdd2e3e7f38b70e1208e23abd54db8b2476e436
Theo Ford <theo.e.ford@gmail.com>
2025-04-08 15:16:43 +0100

    fix for portrait images full bleed homepage

--- src/slices/Image/index.tsx
+++ src/slices/Image/index.tsx
@@ -57,25 +57,7 @@ const Image: FC<ImageProps> = ({ slice }) => {
           </div>
         </div>
       )}
-      {slice.variation == "1XPortraitImage" && (
-        <div className="h-[calc(100vh)] w-[75vw+20px] xl:h-[calc(100vh-20px)] xl:w-[100%] xl:m-[10px]">
-          <div className="w-[calc(100%)] h-[calc(100%)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[10px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-20px)]">
-            {slice.primary.position == "Top" ? (
-              <div className="col-span-4 col-start-1 row-start-1 row-span-12 overflow-hidden xl:col-span-4 xl:row-start-1 xl:row-span-5 xl:col-start-9 flex items-center">
-                <PrismicNextImage
-                  field={slice.primary.image}
-                  alt=""
-                  className="w-auto h-[100%] max-w-none xl:max-w-auto xl:w-[100%] xl:h-auto"
-                />
-              </div>
-            ) : (
-              <div className="col-span-4 col-start-1 row-start-1 row-span-12 overflow-hidden xl:col-span-4 xl:row-start-4 xl:row-span-5 xl:col-start-9  flex items-center">
-                <PrismicNextImage field={slice.primary.image} alt="" />
-              </div>
-            )}
-          </div>
-        </div>
-      )}
+
       {slice.variation == "2XPortraitImage" && (
         <div className="h-[calc(100vh-20px)] w-[100%-20px] m-[10px]">
           <div className="w-[calc(100%)] h-[calc(100%)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[10px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-20px)]">
@@ -97,6 +79,26 @@ const Image: FC<ImageProps> = ({ slice }) => {
                   <PrismicNextImage field={slice.primary.image_2} alt="" />
                 </div>
               </>
+            )}
+          </div>
+        </div>
+      )}
+      {slice.variation == "1XPortraitImage" && (
+        <div className="h-[calc(100vh)]  w-[75vw+20px] xl:h-[calc(100vh-20px)] xl:w-[100%] xl:m-[10px]">
+          {/* <div className="h-[calc(100vh)]  w-[75vw+20px]"> */}
+          <div className="w-[calc(100%)] h-[calc(100%)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[10px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-20px)]">
+            {slice.primary.position == "Top" ? (
+              <div className="col-span-4 col-start-1 row-start-1 row-span-12 overflow-hidden xl:col-span-4 xl:row-start-1 xl:row-span-5 xl:col-start-9 flex items-center">
+                <PrismicNextImage
+                  field={slice.primary.image}
+                  alt=""
+                  className="min-h-[100%] w-auto xl:max-w-auto xl:w-[100%] xl:h-auto"
+                />
+              </div>
+            ) : (
+              <div className="col-span-4 col-start-1 row-start-1 row-span-12 overflow-hidden xl:col-span-4 xl:row-start-4 xl:row-span-5 xl:col-start-9  flex items-center">
+                <PrismicNextImage field={slice.primary.image} alt="" />
+              </div>
             )}
           </div>
         </div>
