2cbc2baedad08064cf681acb1e333eaf34e08f24
Theo Ford <theo.e.ford@gmail.com>
2025-06-29 17:12:36 +0100

    Homepage project - image grid layer 1

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -31,7 +31,7 @@ export default function Carousel({ children }: { children: any }) {
       </Swiper> */}
 
       <Swiper
-        autoplay={{ delay: 1750 }}
+        // autoplay={{ delay: 1750 }}
         speed={1}
         loop={true}
         modules={[Navigation, Autoplay]}
