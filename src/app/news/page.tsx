d7fd71572c32f701b69a33d85858c08da2d0a225
Theo Ford <theo.e.ford@gmail.com>
2025-06-29 16:51:19 +0100

    news height

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -19,7 +19,7 @@ export default async function Page() {
       <Nav />
 
       {/* <SliceZone slices={page.data.slices} components={components} /> */}
-      <div className="ml-[12.5px]">
+      <div className="ml-[15px]">
         <HoriztonalScroll_v0>
           <GridNewsMobileNavSpacer />
 
@@ -29,7 +29,7 @@ export default async function Page() {
                 className="relative float-left h-[calc(83.33vh-0px)] xl:h-[100vh]"
                 key={index}
               >
-                <div className="absolute z-[100] grid grid-cols-4 grid-rows-9 h-[calc(83.33vh-30px)] ml-[12.5px] xl:ml-[15px] gap-x-[12.5px] xl:gap-x-[15px] w-[calc(100vw-50px)] xl:w-[calc(25vw-15px)] xl:h-[calc(100vh-50px)] mt-[25px] xl:mt-[30px] xl:grid-rows-8 text-gray-300">
+                <div className="absolute z-[100] grid grid-cols-4 grid-rows-9 xl:grid-rows-8 h-[calc(83.33vh-30px)] xl:h-[calc(100vh-60px)] w-[calc(100vw-50px)] xl:w-[calc(25vw-15px)] ml-[12.5px] xl:ml-[15px] gap-x-[12.5px] xl:gap-x-[15px]  mt-[25px] xl:mt-[30px] text-gray-300">
                   <div
                     key={"title" + index}
                     className="col-span-4 row-span-1 row-start-1 mt-[-5px] xl:row-start-2 xl:mt-[9vh]"
