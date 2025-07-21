bec01e83ac86043ee5a7d32f832e95a4676e4a1e
Theo Ford <theo.e.ford@gmail.com>
2025-07-21 17:51:49 +0100

    padding right homepage desktop 1

--- src/app/components/HorizontalScroll_v0.tsx
+++ src/app/components/HorizontalScroll_v0.tsx
@@ -39,7 +39,7 @@ export default function HorizontalScroll_v0({
       <div className="w-[100vw] overflow-x-hidden xl:overflow-x-scroll xl:overflow-y-hidden">
         <div
           ref={scrollRef}
-          className="overflow-hidden xl:flex xl:flex-nowrap xl:overflow-x-scroll"
+          className="overflow-hidden xl:flex xl:flex-nowrap xl:overflow-x-scroll last:xl:pr-[30px]"
           // style={{
           //   scrollSnapType: "x proximity",
           //   scrollBehavior: "smooth"
