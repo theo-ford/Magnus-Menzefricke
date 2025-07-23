7ce94288e7d08be61932e6485ba9a65f107c83b5
Theo Ford <theo.e.ford@gmail.com>
2025-07-23 15:49:53 +0100

    hero text 2

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -48,12 +48,14 @@ export default function Hero({ pageData }) {
             </div>
           </div>
           <div className="col-span-4 col-start-1 row-start-10 xl:col-start-1 xl:col-span-8 xl:row-start-7">
-            <h5 className="text-[18px] p-[0px] mt-[-10px] xl:mt-[-6px] leading-none  xl:text-[24px]">
+            {/* <h5 className="text-[18px] p-[0px] mt-[-10px] xl:mt-[-6px] leading-none  xl:text-[24px]"> */}
+            <h5 className="text-[18px] p-[0px] mt-[-10px] xl:mt-[-0.4vh] leading-none  xl:text-[24px]">
               <PrismicRichText field={pageData.introduction} />
             </h5>
           </div>
           <div className="col-span-4 col-start-1 row-start-10 xl:col-start-10 xl:col-span-3 xl:row-start-7 hidden xl:inline">
-            <h5 className="text-[18px] p-[0px] mt-[-1px] xl:mt-[-6px] leading-none  xl:text-[18px]">
+            {/* <h5 className="text-[18px] p-[0px] mt-[-1px] xl:mt-[-6px] leading-none  xl:text-[18px]"> */}
+            <h5 className="text-[18px] p-[0px] mt-[-10px] xl:mt-[-0.4vh] leading-none  xl:text-[24px]">
               <p>
                 {pageData.website_url}
                 <br></br>
