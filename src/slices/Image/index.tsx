499cf04e4cada6b42998170affc80a9d96a594be
Theo Ford <theo.e.ford@gmail.com>
2025-07-28 17:03:49 +0100

    homepage 3

--- src/slices/Image/index.tsx
+++ src/slices/Image/index.tsx
@@ -27,10 +27,10 @@ const Image: FC<ImageProps> = ({ slice }) => {
         // <div className="w-[100%] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%)]">
         // <div className=" h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] w-[calc(100%-2px)] xl:w-[calc(100%-1.5px)] xl:mt-[0px]">
         <div className=" h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] w-[calc(100%-2px)] xl:w-[calc(100%-1.5px)] xl:mt-[0px]">
-          {/* <div className="w-[calc(100%-22px)] xl:w-[calc(75vw-15px)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]">
+          {/* <div className="w-[calc(100%-22px)] xl:w-[calc(75vw-15px)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]">
             {slice.primary.position == "Left" ? (
               <div className="col-span-4 col-start-1 row-start-5 row-span-4 md:col-span-2 xl:!col-span-4 xl:row-start-4 xl:row-span-2 xl:!col-start-1 flex items-center justify-center overflow-hidden">
                 <PrismicNextImage
@@ -58,10 +58,10 @@ const Image: FC<ImageProps> = ({ slice }) => {
         // <div className="w-[100%] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%)]">
         // <div className="w-[100%] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%-1.5px)] xl:mt-[0px]">
         <div className="w-[100%] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%-1.5px)] xl:mt-[0px]">
-          {/* <div className="w-[calc(100%-22px)] xl:w-[calc(75vw-15px)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]">
+          {/* <div className="w-[calc(100%-22px)] xl:w-[calc(75vw-15px)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]">
             {slice.primary.position == "Left" ? (
               <>
                 <div className="col-span-2 col-start-1 row-start-6 row-span-2 md:row-span-4 md:row-start-5 xl:col-span-4 xl:!row-start-4 xl:!row-span-2 xl:col-start-1 overflow-hidden flex items-center">
@@ -108,10 +108,10 @@ const Image: FC<ImageProps> = ({ slice }) => {
         // <div className="w-[100%] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%)]">
         // <div className="w-[100%] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%-1.5px)] xl:mt-[0px]">
         <div className="w-[100%] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%-1.5px)] xl:mt-[0px]">
-          {/* <div className="w-[calc(100%-22px)] xl:w-[calc(75vw-15px)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]">
+          {/* <div className="w-[calc(100%-22px)] xl:w-[calc(75vw-15px)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]">
             {slice.primary.position == "Top" ? (
               <>
                 <div className="col-span-2 col-start-1 row-start-5 row-span-4 md:row-span-6 md:row-start-4 xl:col-span-4 xl:!row-start-1 xl:!row-span-5 xl:col-start-5 flex items-center overflow-hidden">
@@ -160,10 +160,10 @@ const Image: FC<ImageProps> = ({ slice }) => {
         // <div className="w-[100%] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%)]">
         // <div className="w-[100%] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%-1.5px)] xl:mt-[0px]">
         <div className="w-[100%] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] xl:w-[calc(100%-1.5px)] xl:mt-[0px]">
-          {/* <div className="w-[calc(100%-22px)] xl:w-[calc(75vw-15px)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
-          <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]">
+          {/* <div className="w-[calc(100%-22px)] xl:w-[calc(75vw-15px)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-60px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          {/* <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-44px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]"> */}
+          <div className="w-[calc(100%)] xl:w-[calc(100%)] h-[calc(100vh-52px)] xl:h-[calc(100vh-63px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[17px] gap-y-[2px] xl:grid-cols-12 xl:grid-rows-8 xl:w-[calc(75vw-15px)]">
             {slice.primary.position == "Top" ? (
               // <div className="col-span-4 col-start-1 row-start-1 row-span-12 overflow-hidden xl:col-span-4 xl:row-start-1 xl:row-span-5 xl:col-start-9 flex items-center">
               <div className="col-span-2 col-start-1 row-start-5 row-span-4 overflow-hidden xl:col-span-4 xl:!row-start-1 xl:row-span-5 xl:col-start-9 flex items-center">
@@ -188,7 +188,7 @@ const Image: FC<ImageProps> = ({ slice }) => {
       )}
       {/* {slice.variation == "fullBleedImage" && (
         <div className="w-[100%] h-[calc(100vh-44px)] m-[0px] xl:mt-[0px] xl:h-[calc(100vh-60px)] overflow-hidden xl:w-[calc(75vw)]">
-          <div className="w-[calc(100%)] h-[calc(100%-0px)] xl:h-[calc(100%+0px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[9px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 ">
+          <div className="w-[calc(100%)] h-[calc(100%-0px)] xl:h-[calc(100%+0px)] float-left grid grid-cols-4 grid-rows-12 relative gap-x-[11px] xl:gap-x-[15px] gap-y-[0px] xl:grid-cols-12 xl:grid-rows-8 ">
             <div className="col-span-4 col-start-1 row-start-1 row-span-12 overflow-hidden xl:row-span-8 xl:col-span-12 xl:col-start-1 l:row-start-1 flex items-center justify-center">
               <PrismicNextImage
                 field={slice.primary.desktop_image}
